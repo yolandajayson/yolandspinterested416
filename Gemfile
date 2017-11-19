ff8b51f7e0f36dfe440d80916d77110a9f7cc963
Yolanda Jayson <yolanda.jayson@gmail.com>
2017-11-19 22:56:29 +0000

    Add Devise

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',         group: :doc
 gem 'spring',         group: :development
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
-
+gem 'devise', '~> 4.3'
 
 
 group :development, :test do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Added per Devise installation instructions
+  config.action_mailer.default_url_options = { host: 'yolandaspinterested416.herokuapp.com '}
 end
