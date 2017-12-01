7ad0a1d84084c4c575182e863561d75509d6c740
Yolanda Jayson <yolanda.jayson@gmail.com>
2017-12-01 04:15:58 +0000

    added s3 to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,14 @@ Rails.application.configure do
   
   # Added per Devise installation instructions
   config.action_mailer.default_url_options = { host: 'yolandaspinterested416.herokuapp.com '}
+  
+  config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch['S3_BUCKET_NAME'],
+    access_key_id: ENV.fetch['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV.fetch['AWS_SECRET_ACCESS_KEY'],
+  }
+}
+  
 end
