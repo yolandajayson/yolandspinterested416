bdf237fec2c43689ae8e8b127e16ef9f96684d42
Yolanda Jayson <yolanda.jayson@gmail.com>
2017-12-01 08:29:27 +0000

    added s3 to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,11 +81,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'yolandaspinterested416.herokuapp.com '}
   
   config.paperclip_defaults = {
-  storage: :s3,
-  s3_credentials: {
-    bucket: ENV.fetch['S3_BUCKET_NAME'],
-    access_key_id: ENV.fetch['AWS_ACCESS_KEY_ID'],
-    secret_access_key: ENV.fetch['AWS_SECRET_ACCESS_KEY'],
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV.fetch['S3_BUCKET_NAME'],
+    :access_key_id => ENV.fetch['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV.fetch['AWS_SECRET_ACCESS_KEY'],
   }
 }
   
